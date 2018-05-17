c9bb14e163ae9dabc5270207c6dd3dbd749f8b05
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-05-17 15:48:02 +0900

    Fix the installation script of Ansible
    Answer 'yes' for all question when the it was executed `apt-add-repository`

--- install-ansible.sh
+++ install-ansible.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y "ppa:ansible/ansible"
 sudo apt-get update
 sudo apt-get install ansible
